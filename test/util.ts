f51f78df02c61af389fec76761856ddb91d8e351
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-08-20 02:02:18 -0700

    use TS function overloads for stringToBytes32

--- test/util.ts
+++ test/util.ts
@@ -2,8 +2,9 @@ import { Bytes, BytesLike, ethers } from "ethers";
 const strToB32 = ethers.utils.formatBytes32String;
 const parseB32Str = ethers.utils.parseBytes32String;
 
-export function stringToBytes32 (input: string | string[])
-: string | string[] {
+export function stringToBytes32(input: string): string;
+export function stringToBytes32(input: string[]): string[];
+export function stringToBytes32 (input: string | string[]): string | string[] {
   return Array.isArray(input) ? input.map(strToB32) : strToB32(input);
 }
 
