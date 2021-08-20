d39e0983aa49e9e6213f56301d243ef5ed1437a7
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-08-20 00:57:33 -0700

    Update util.js

--- test/util.js
+++ test/util.js
@@ -1,17 +1,13 @@
-const hre = require("hardhat");
-const web3 = hre.web3;
-
-function strToB32(str) {
-  var hexStr = web3.utils.asciiToHex(str);
-  //console.log("string:", str, "as hex:", hexStr, "hex length:", hexStr.length);
-  if(hexStr.length > 64)
-    throw new Exception("maximum length of hex string is 64, but " + str + "has length " + hexStr.length);
-
-  return web3.utils.padLeft(hexStr, 64);
-}
+const ethers = require("ethers");
+const strToB32 = ethers.utils.formatBytes32String;
+const parseB32Str = ethers.utils.parseBytes32String;
 
 module.exports = {
     stringToBytes32: function (input) {
       return Array.isArray(input) ? input.map(strToB32) : strToB32(input);
+    },
+
+    bytes32ToString: function (input) {
+      return Array.isArray(input) ? input.map(parseB32Str) : parseB32Str(input);
     }
 }
