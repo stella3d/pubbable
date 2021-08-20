1bf969d90ca3f1b4c6091dd710e7c97310bfe0c5
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-08-19 20:14:34 -0700

    add stub for cocktail factory test

---
+++ test/SimpleCocktailFactory-test.js
@@ -0,0 +1,15 @@
+
+const util = require("./util.js");
+
+describe("SimpleCocktailFactory", function () {
+    let Factory;
+
+    before(async () => {
+        Factory = await ethers.getContractFactory("SimpleCocktailFactory");
+    });
+  
+    it("Should ???????????????", async function () {
+        
+    });
+  });
+  
