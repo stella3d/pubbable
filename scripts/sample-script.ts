acd15eaf44742f43a20c36fb622a6775604ce455
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-08-23 20:07:34 -0700

    convert sample script to typescript

--- scripts/sample-script.ts
+++ scripts/sample-script.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import hre from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
