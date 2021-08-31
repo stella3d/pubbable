735b9102ec85928bde6eff7570db7b46f25dcb1d
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-08-30 22:14:14 -0700

    add bytes32 field comment in Cocktail

--- contracts/Pubbable.sol
+++ contracts/Pubbable.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract Pubbable is ERC1155 {
 
     struct Cocktail {
+        // use bytes32 instead of strings to lower gas costs
         bytes32 name;
         bytes32[] ingredients;
     }
